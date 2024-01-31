5dc92f650da3226627abddbff544d2f68f54b12d
sutti <suttipong.ta@rmuti.ac.th>
2024-01-31 16:26:39 +0700

    fix product controller

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -1,13 +1,61 @@
-exports.getProductsLanding = (req, res, next) => {
-  res.json({ message: "Get Product Landing Page" });
+const prisma = require("../config/prisma");
+const createError = require("../utils/createError");
+
+exports.getProductsLanding = async (req, res, next) => {
+  try {
+    const products = await prisma.product.findMany();
+    res.json({ products });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.getProducts = (req, res, next) => {
-  const { promotion, brand, collection, series } = req.query;
-  res.json({ promotion, brand, collection, series });
+exports.getProducts = async (req, res, next) => {
+  try {
+  const { brand, collection, series } = req.query;
+  const products = await prisma.product.findMany({
+    where: {
+      brand: {
+        name: brand,
+      },
+      collection: {
+        name: collection,
+      },
+      series: {
+        name: series,
+      }
+    },
+    include: {
+      brand: true,
+      collection: true,
+      series: true,
+    },
+  });
+  if(products.length === 0) {
+    return createError(404, 'Product not found');
+  }
+  res.json({ products });
+  } catch (err) {
+    next(err);
+  }
 };
 
-exports.getProductById = (req, res, next) => {
-  const { productId } = req.params;
-  res.json({ productId });
+exports.getProductById = async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+    if (!product) {
+      return createError(404, "Product ID not found");
+    }
+    const product = await prisma.product.findFirst({
+      where: {
+        id: Number(productId),
+      },
+    });
+    if (product === null) {
+      return createError(400, "Product ID = " + productId + " have no item");
+    }
+    res.json({ product });
+  } catch (err) {
+    next(err);
+  }
 };
